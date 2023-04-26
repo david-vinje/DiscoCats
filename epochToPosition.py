f3dc4486e95625d876b05f4f6fa8a46f902d207e
davv <davv@itu.dk>
2023-04-26 13:56:00 +0200

    update positions

--- epochToPosition.py
+++ epochToPosition.py
@@ -7,26 +7,27 @@ by_name = {sat.name: sat for sat in satellites}
 satellite = by_name[name]
 
 ts = load.timescale()
-lines = open('epochs.txt', 'r').readlines()
-first_line = lines[0]
+lines = [x.rstrip('\n') for x in open('epochs.txt', 'r').readlines()]
+n = len(lines) - 1
+first_line = lines[n]
 positions_file = open('positions.txt', 'a')
     
-latest_timestamp_file = open('last_timestamp', 'r')
+latest_timestamp_file = open('latest_timestamp.txt', 'r')
 latest_timestamp = latest_timestamp_file.readline()
 latest_timestamp_file.close()
 
-i = 0
+i = n
 while lines[i] != latest_timestamp:
-    i += 1
+    i -= 1
+    line = lines[i]
     time = lines[i].split(' ')
     year, month, day = [int(x) for x in time[0].split('-')]
     hour, minute, second = [int(x) for x in time[1].split(':')]
     time = ts.tt(year, month, day, hour, minute, second)
-    pos, vel = satellite.at(time).position.km, satellite.at(time).velocity.km_per_s
-    x, y, z = pos
-    positions_file.write(''.join((str(pos), str(vel))) + '\n')
-
-latest_timestamp_file = open('last_timestamp', 'w')
-latest_timestamp_file.write(first_line)
-
+    pos = [str(x) for x in satellite.at(time).position.km]
+    positions_file.write(' '.join(pos) + '\n')
 positions_file.close()
+
+latest_timestamp_file = open('latest_timestamp.txt', 'w')
+latest_timestamp_file.write(first_line)
+latest_timestamp_file.close()
